a59ca059bd2f0743fd8b0c561db59f74cf3c4696
wyminc <wyminc808@gmail.com>
2018-08-15 22:26:50 -1000

    fixed up some code to look cleaner

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const { hydrogen } = require("./assets/hydrogen.js");
 const { index } = require("./assets/index.js");
 const { css } = require("./assets/styles.js");
 
-// console.log(process.argv);
-
 const server = net.createServer(client => {
   client.setEncoding("utf8");
   client.on("data", data => {
@@ -20,38 +18,36 @@ const server = net.createServer(client => {
     const status = "HTTP/1.1 200 OK"; // it's all good
     const serverName = "coolServer"; // whatever server
 
-    console.log(uri);
-
-    if (uri === "/helium") {
-
-      const message = `${status}\n Server: ${serverName}\n Date: ${date}\n Content-Type: text/html\n\n${helium}`;
-
-      client.write(message);
-      client.end();
-    } else if (uri === "/hydrogen") {
-      const message = `${status}\n Server: ${serverName}\n Date: ${date}\n Content-Type: text/html\n\n${hydrogen}`;
-
-      client.write(message);
-      client.end();
-    } else if (uri === "/index" || uri === "/") {
+    if (uri === "/") {
       const message = `${status}\n Server: ${serverName}\n Date: ${date}\n Content-Type: text/html\n\n${index}`;
 
       client.write(message);
       client.end();
-    } else if (uri === "/404") {
-      const message = `${status}\n Server: ${serverName}\n Date: ${date}\n Content-Type: text/html\n\n${error}`;
+
+    } else if (uri === "/helium.html") {
+      const message = `${status}\n Server: ${serverName}\n Date: ${date}\n Content-Type: text/html\n\n${helium}`;
 
       client.write(message);
       client.end();
+
+    } else if (uri === "/hydrogen.html") {
+      const message = `${status}\n Server: ${serverName}\n Date: ${date}\n Content-Type: text/html\n\n${hydrogen}`;
+
+      client.write(message);
+      client.end();
+
     } else if (uri === "/css/styles.css") {
-
-
       const message = `${status}\nServer: ${serverName}\n Date: ${date}\n Content-Type: text/css\n\n${css}`;
 
       client.write(message);
       client.end;
-    }
 
+    } else {
+      const message = `${status}\n Server: ${serverName}\n Date: ${date}\n Content-Type: text/html\n\n${error}`;
+
+      client.write(message);
+      client.end();
+    }
   });
 });
 
